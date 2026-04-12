58be5e6a5c65fefd3aa3b5d83d35c24e92db276f
Toba <270317238+bl4-k@users.noreply.github.com>
2026-04-12 19:58:57 +0100

    chore: intial working java 21 enviroment

--- src/main/java/com/automation/App.java
+++ src/main/java/com/automation/App.java
@@ -1,13 +1,22 @@
 package com.automation;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class App {
+    public static void main(String[] args) {
+        // Path to the downloads - might expand in future to any selected folder
+        Path downloadsPath = Paths.get(System.getProperty("user.home"), "Downloads");
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(downloadsPath)) {
+            for (Path entry : stream) {
+                System.out.println("Found file: " + entry.getFileName());
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading folder: " + e.getMessage());
+        }
     }
 }
