7d10ce404937c31650d94157fc0cec338f5a9757
Toba <270317238+bl4-k@users.noreply.github.com>
2026-04-13 16:28:39 +0100

    feature: added directory chooser to user interface

--- src/main/java/com/automation/UIController.java
+++ src/main/java/com/automation/UIController.java
@@ -1,12 +1,22 @@
 package com.automation;
 
+import java.io.File;
+import java.nio.file.Path;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
 public class UIController {
 
+    private Path selectedDir;
+
     @FXML
     private Label statusLabel;
+
+    @FXML
+    private Label pathLabel;
 
     @FXML
     private void handleRun() {
@@ -16,6 +26,19 @@ public class UIController {
         organiser.organiseDownloads();
 
         statusLabel.setText("Status: Done! Check your folders.");
+    }
+
+    @FXML
+    private void handleSelectFolder() {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select Folder to Organise");
+
+        File selectedFile = directoryChooser.showDialog(new Stage());
+        
+        if (selectedFile != null) {
+            selectedDir = selectedFile.toPath();
+            pathLabel.setText(selectedDir.toString());
+        }
     }
     
 }
